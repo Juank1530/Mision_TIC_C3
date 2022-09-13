34479a8bfdc52465e72093ae4413472bc2c9973a
juank153021@gmail.com <juank153021@gmail.com>
2022-09-13 00:42:26 -0500

    DB connect successfully

--- Proyecto-Ciclo3.App/ProyectoCiclo3.App.Persistencia/AppRepositorios/RepositorioBuses.cs
+++ Proyecto-Ciclo3.App/ProyectoCiclo3.App.Persistencia/AppRepositorios/RepositorioBuses.cs
@@ -2,64 +2,55 @@ using System.Collections.Generic;
 using ProyectoCiclo3.App.Dominio;
 using System.Linq;
 using System;
-
-
+ 
 namespace ProyectoCiclo3.App.Persistencia.AppRepositorios
 {
     public class RepositorioBuses
     {
         List<Buses> buses;
-    public RepositorioBuses()
-        {
-            buses= new List<Buses>()
-            {
-                new Buses{id=1,marca="Mercedes Benz",modelo= 2000,kilometraje= 4000,num_asientos= 40, placa= "ACB458"},
-                new Buses{id=2,marca="KIA",modelo= 2015,kilometraje= 4500,num_asientos= 45, placa= "SDE852"},
-                new Buses{id=3,marca="Ford",modelo= 2022,kilometraje= 6000,num_asientos= 60, placa= "WSA545"}
-            };
-        }
-
+        private readonly AppContext _appContext = new AppContext();
+ 
         public IEnumerable<Buses> GetAll()
         {
-            return buses;
+            return _appContext.Buses;
         }
-
+ 
         public Buses GetWithId(int id){
-            return buses.SingleOrDefault(b => b.id == id);
+            return _appContext.Buses.Find(id);
         }
 
-        //Esto se pego acá nuevo y lo paso el profe
         public Buses Update(Buses newBus){
-            var bus= buses.SingleOrDefault(b => b.id == newBus.id);
+            var bus = _appContext.Buses.Find(newBus.id);;
             if(bus != null){
                 bus.marca = newBus.marca;
                 bus.modelo = newBus.modelo;
                 bus.kilometraje = newBus.kilometraje;
                 bus.num_asientos = newBus.num_asientos;
                 bus.placa = newBus.placa;
+                //Guardar en base de datos
+                 _appContext.SaveChanges();
             }
         return bus;
         }
 
-        public Buses Create(Buses newBus)
+        public Buses Create(Buses newbus)
         {
-           if(buses.Count > 0){
-           newBus.id=buses.Max(r => r.id) +1; 
-            }else{
-               newBus.id = 1; 
-            }
-           buses.Add(newBus);
-           return newBus;
+            var addBus = _appContext.Buses.Add(newbus);
+            //Guardar en base de datos
+            _appContext.SaveChanges();
+            return addBus.Entity;
         }
 
         public Buses Delete(int id)
         {
-            var bus= buses.SingleOrDefault(b => b.id == id);
-            buses.Remove(bus);
-            return bus;
+            var bus = _appContext.Buses.Find(id);
+            if (bus != null){
+                _appContext.Buses.Remove(bus);
+                //Guardar en base de datos
+                _appContext.SaveChanges();
+            }
+            return null;
         }
-
-
 
     }
 }
--- Proyecto-Ciclo3.App/ProyectoCiclo3.App.Persistencia/AppRepositorios/RepositorioEstaciones.cs
+++ Proyecto-Ciclo3.App/ProyectoCiclo3.App.Persistencia/AppRepositorios/RepositorioEstaciones.cs
@@ -2,67 +2,55 @@ using System.Collections.Generic;
 using ProyectoCiclo3.App.Dominio;
 using System.Linq;
 using System;
-
-
+ 
 namespace ProyectoCiclo3.App.Persistencia.AppRepositorios
 {
     public class RepositorioEstaciones
     {
         List<Estaciones> estaciones;
-    public RepositorioEstaciones()
-        {
-            estaciones= new List<Estaciones>()
-            {
-                new Estaciones{id=1,nombre="Estación Centro",direccion= "Calle 36 98 56",Coord_x= 10.458,Coord_y= 25.654, tipo= "Principal"},
-                new Estaciones{id=2,nombre="Estación Estadio",direccion= "Calle 98 56 36",Coord_x= 12.859,Coord_y= 45.896, tipo= "Secundaria"},
-                new Estaciones{id=3,nombre="Estación Terminal",direccion= "Carrera 78 59 78",Coord_x= 15.547,Coord_y= 58.658, tipo= "Principal"}
-            };
-        }
-
+        private readonly AppContext _appContext = new AppContext();
+ 
         public IEnumerable<Estaciones> GetAll()
         {
-            return estaciones;
+            return _appContext.Estaciones;
         }
-
+ 
         public Estaciones GetWithId(int id){
-            return estaciones.SingleOrDefault(b => b.id == id);
+            return _appContext.Estaciones.Find(id);
         }
 
-        //Esto se pego acá nuevo y lo paso el profe 
         public Estaciones Update(Estaciones newEstacion){
-            var estacion= estaciones.SingleOrDefault(b => b.id == newEstacion.id);
+            var estacion = _appContext.Estaciones.Find(newEstacion.id);;
             if(estacion != null){
                 estacion.nombre = newEstacion.nombre;
                 estacion.direccion = newEstacion.direccion;
                 estacion.Coord_x = newEstacion.Coord_x;
                 estacion.Coord_y = newEstacion.Coord_y;
                 estacion.tipo = newEstacion.tipo;
+                //Guardar en base de datos
+                 _appContext.SaveChanges();
             }
         return estacion;
         }
 
         public Estaciones Create(Estaciones newEstacion)
         {
-           if(estaciones.Count > 0){
-           newEstacion.id=estaciones.Max(r => r.id) +1; 
-            }else{
-               newEstacion.id = 1; 
-            }
-           estaciones.Add(newEstacion);
-           return newEstacion;
+            var addEstacion = _appContext.Estaciones.Add(newEstacion);
+            //Guardar en base de datos
+            _appContext.SaveChanges();
+            return addEstacion.Entity;
         }
 
         public Estaciones Delete(int id)
         {
-            var estacion= estaciones.SingleOrDefault(e => e.id == id);
-            estaciones.Remove(estacion);
-            return estacion;
+            var estacion = _appContext.Estaciones.Find(id);
+            if (estacion != null){
+                _appContext.Estaciones.Remove(estacion);
+                //Guardar en base de datos
+                _appContext.SaveChanges();
+            }
+            return null;
         }
-
-
-
-
-
 
     }
 }
